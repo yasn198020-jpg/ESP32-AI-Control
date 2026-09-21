9825d8ed66038c13c553dac942d8d5c93c019714
yasn198020-jpg <yasn198020@gmail.com>
2026-09-21 07:26:09 +0300

    Fix experimental Material3 API compilation

--- app/src/main/java/com/yasn198020/aicontrol/MainActivity.kt
+++ app/src/main/java/com/yasn198020/aicontrol/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +28,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun App() {
     val context = LocalContext.current
