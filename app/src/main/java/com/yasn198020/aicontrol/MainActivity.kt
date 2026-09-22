9595a86cb130d229519e258510fd75aa8287f148
yasn198020-jpg <yasn198020@gmail.com>
2026-09-23 00:34:58 +0300

    Fix installed TTS voice screen build

--- app/src/main/java/com/yasn198020/aicontrol/MainActivity.kt
+++ app/src/main/java/com/yasn198020/aicontrol/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.ExperimentalFoundationApi
