83254ac5368e4bd40583c71fca0455db653b73a4
yasn198020-jpg <yasn198020@gmail.com>
2026-09-23 19:38:58 +0300

    Optimize scenario tile click response

--- app/src/main/java/com/yasn198020/aicontrol/ScenariosScreen.kt
+++ app/src/main/java/com/yasn198020/aicontrol/ScenariosScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
