d970413208de913ab220cd29a89e8d2871e504b6
yasn198020-jpg <yasn198020@gmail.com>
2026-09-23 06:53:56 +0300

    Fix Density import for adjustable text size

--- app/src/main/java/com/yasn198020/aicontrol/MainActivity.kt
+++ app/src/main/java/com/yasn198020/aicontrol/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
