1dd0d76de3e8ab9f383dd2c8d6b69954241e4646
yasn198020-jpg <yasn198020@gmail.com>
2026-09-23 18:28:11 +0300

    Fix scenario editor scrolling import

--- app/src/main/java/com/yasn198020/aicontrol/ScenariosScreen.kt
+++ app/src/main/java/com/yasn198020/aicontrol/ScenariosScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
