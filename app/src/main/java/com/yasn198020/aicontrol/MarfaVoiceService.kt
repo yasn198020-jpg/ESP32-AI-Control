426a0d066f73e0dbe352c304f54f0f2c2703630c
yasn198020-jpg <yasn198020@gmail.com>
2026-09-23 22:25:30 +0300

    fix: import extracted device models

--- app/src/main/java/com/yasn198020/aicontrol/MarfaVoiceService.kt
+++ app/src/main/java/com/yasn198020/aicontrol/MarfaVoiceService.kt
@@ -1,4 +1,6 @@
 package com.yasn198020.aicontrol
+import com.yasn198020.aicontrol.core.Device
+import com.yasn198020.aicontrol.core.WidgetState
 
 import android.app.Notification
 import android.app.NotificationChannel
