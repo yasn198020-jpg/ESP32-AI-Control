01b272d772fc412a32688ff9f92b233ea075ff32
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 13:27:52 +0300

    chore: tidy training helper imports

--- app/src/main/java/com/yasn198020/aicontrol/AppTrainingHelpers.kt
+++ app/src/main/java/com/yasn198020/aicontrol/AppTrainingHelpers.kt
@@ -1,6 +1,7 @@
 package com.yasn198020.aicontrol
 
 import com.yasn198020.aicontrol.core.WidgetState
+
 import com.yasn198020.aicontrol.voice.TRAINED_READ_VALUE
 
 /** Returns whether a widget can be attached to a trained voice command. */
