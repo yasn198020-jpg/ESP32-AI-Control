87d6a20c61fb8c1d27cd1d2cf024eb372ec2e370
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 15:26:33 +0300

    refactor: centralize app intent constants

---
+++ app/src/main/java/com/yasn198020/aicontrol/AppIntents.kt
@@ -0,0 +1,5 @@
+package com.yasn198020.aicontrol
+
+const val ACTION_MARFA_SHORTCUT = "com.yasn198020.aicontrol.action.MARFA_SHORTCUT"
+const val EXTRA_MARFA_SHORTCUT_TOGGLE = "marfa_shortcut_toggle"
+const val EXTRA_START_LISTENING = "start_listening"
