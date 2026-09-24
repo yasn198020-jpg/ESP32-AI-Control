d6dbbf1deb7e2904c96da45e36084027f8354425
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 13:06:10 +0300

    refactor: remove unused update dialog import

--- app/src/main/java/com/yasn198020/aicontrol/UpdateDialog.kt
+++ app/src/main/java/com/yasn198020/aicontrol/UpdateDialog.kt
@@ -6,7 +6,6 @@ import android.net.Uri
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import com.yasn198020.aicontrol.updates.UpdateManager
 
