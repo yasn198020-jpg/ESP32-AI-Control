c250f233b6d12e29c916b44350a3054f6034c99a
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 09:50:59 +0300

    refactor: remove redundant App imports

--- app/src/main/java/com/yasn198020/aicontrol/App.kt
+++ app/src/main/java/com/yasn198020/aicontrol/App.kt
@@ -22,9 +22,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.clickable
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.material3.*
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
-import kotlinx.coroutines.delay
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
