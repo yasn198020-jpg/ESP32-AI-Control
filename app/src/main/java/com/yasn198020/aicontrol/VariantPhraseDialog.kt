11e15060802643d2bd52a96fc472b739c11eafdd
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 12:40:00 +0300

    fix: import dialog spacing unit

--- app/src/main/java/com/yasn198020/aicontrol/VariantPhraseDialog.kt
+++ app/src/main/java/com/yasn198020/aicontrol/VariantPhraseDialog.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun VariantPhraseDialog(
