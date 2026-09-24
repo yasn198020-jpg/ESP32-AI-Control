bd3710ac72bd0789c1d39f753a597c643519d2a2
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 12:21:45 +0300

    refactor: move training widget helpers out of App

--- app/src/main/java/com/yasn198020/aicontrol/App.kt
+++ app/src/main/java/com/yasn198020/aicontrol/App.kt
@@ -162,19 +162,6 @@ fun App(
         voiceStatus = "Настройки голоса сохранены"
     }
 
-    fun isTrainableWidget(widget: WidgetState): Boolean =
-        widget.type == WidgetState.Type.TOGGLE ||
-            widget.type == WidgetState.Type.BUTTON ||
-            widget.type == WidgetState.Type.VALUE ||
-            widget.type == WidgetState.Type.STATUS
-
-    fun trainingValueFor(widget: WidgetState): String =
-        if (widget.type == WidgetState.Type.VALUE || widget.type == WidgetState.Type.STATUS) {
-            TRAINED_READ_VALUE
-        } else {
-            "1"
-        }
-
     fun openTraining(deviceId: String, widget: WidgetState) {
         if (!isTrainableWidget(widget)) return
         trainingTarget = TrainingTarget(deviceId, widget.id, widget.title)
