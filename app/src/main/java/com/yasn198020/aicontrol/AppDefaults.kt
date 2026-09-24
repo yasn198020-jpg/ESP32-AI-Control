0ce018788f97ca18e0fe2dd5bef13607b4e3e9f9
yasn198020-jpg <yasn198020@gmail.com>
2026-09-24 15:17:53 +0300

    refactor: centralize app defaults

---
+++ app/src/main/java/com/yasn198020/aicontrol/AppDefaults.kt
@@ -0,0 +1,6 @@
+package com.yasn198020.aicontrol
+
+const val APP_SETTINGS_PREFS = "settings"
+const val DEFAULT_MQTT_PORT = 1883
+const val MAX_DIAGNOSTIC_LOG_ENTRIES = 100
+const val MARFA_ACTIVE_PREF = "marfa_voice_active"
